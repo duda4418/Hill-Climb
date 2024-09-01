bf45216f400b8c7211e348e6b702da7addab5e7b
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-09-01 23:43:12 +0300

    Adding High Score

--- Assets/Scripts/DisplayDistanceText.cs
+++ Assets/Scripts/DisplayDistanceText.cs
@@ -7,12 +7,16 @@ public class DisplayDistanceText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI distanceText;
     [SerializeField] private Transform playerTrans;
+    [SerializeField] private TextMeshProUGUI highScoreText;
 
+    private int highScore;
     private Vector2 startPosition;
 
     private void Start()
     {
         startPosition = playerTrans.position;
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+        highScoreText.text = "High Score: " + highScore.ToString() + "m";
     }
 
     private void Update()
@@ -23,6 +27,13 @@ public class DisplayDistanceText : MonoBehaviour
         if (distance.x < 0)
         {
             distance.x = 0;
+        }
+
+        if (distance.x > highScore)
+        {
+            highScore = (int)distance.x;
+            highScoreText.text = "High Score: " + highScore.ToString() + "m";
+            PlayerPrefs.SetInt("highScore", highScore);
         }
 
         distanceText.text = "Distance: " + distance.x.ToString("F0") + "m";
